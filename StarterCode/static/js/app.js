4936f927c337ee84791ba873572a80f14123a51e
Rufuz42 <Steven.Rufus42@gmail.com>
2022-09-24 20:23:22 -0400

    Bar Chart Added
    Full bar chart functionality added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,7 +5,7 @@ function initializeDB()
 {
     // Puts all the data in the console log
     let data = d3.json('samples.json');
-    console.log(data);
+    // console.log(data);
 
     // Build the drop down menu with the selDataset ID by feeding in names from samples.json
     let selectData = d3.select('#selDataset');
@@ -26,6 +26,8 @@ function initializeDB()
         // Builds the metadata
         demographicInfo(firstSampleID);
 
+        // Call the function to build the chart chart
+        buildBarChart(firstSampleID);
     });
 
 }
@@ -39,6 +41,9 @@ function optionChanged(item)
     // console.log(item);
     // Call the metadata update
     demographicInfo(item);
+
+    // Call the function to build the bar chart
+    buildBarChart(item);
 }
 
 
@@ -59,7 +64,7 @@ function demographicInfo(sampleID)
 
         // get just index 0 from the array, returns object
         let resultData = resultID[0];
-        console.log(resultData);
+        // console.log(resultData);
 
         // Clear the existing data out before repopulating the table
         d3.select('#sample-metadata').html('');
@@ -74,3 +79,63 @@ function demographicInfo(sampleID)
 
 
 // Build the Graphs here
+function buildBarChart(sampleID)
+{
+    // console.log(sampleID);
+    // let data = d3.json('samples.json');
+    // console.log(data);
+
+    d3.json('samples.json').then((data) => {
+
+        // grabs the sample data
+        let sampleData = data.samples;
+        // console.log(sampleData);
+
+        // filter based on the value of the sampleID, returns array
+        let resultID = sampleData.filter(sampleIDResult => sampleIDResult.id == sampleID);
+        // console.log(resultID);
+
+        // get just index 0 from the array, returns object
+        let resultData = resultID[0];
+        // console.log(resultData);
+
+        // Get the otu_ids, labels, and sample_values values
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+        // console.log(otu_ids);
+        // console.log(otu_labels);
+        // console.log(sample_values);
+
+        // Build out the bar chart
+        let yTicks = otu_ids.slice(0, 10).map(id => `otu ${id}`);
+        // console.log(yTicks);
+        let xValues = sample_values.slice(0, 10);
+        // console.log(xValues);
+        let textLabels = otu_labels.slice(0, 10);
+        // console.log(textLabels);
+
+        let barChart = {
+            y: yTicks.reverse(), // Need it to most to least top to bottom
+            x: xValues.reverse(),
+            text: textLabels.reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }
+
+        let layout = {
+            title: "Top 10 Belly Button Bacteria"
+        };
+
+        Plotly.newPlot('bar', [barChart], layout);
+
+        // // Clear the existing data out before repopulating the table
+        // d3.select('#sample-metadata').html('');
+
+        // // extract the value-key pairs to the chart
+        // Object.entries(resultData).forEach(([key, value]) => {
+        //     d3.select('#sample-metadata').append('h5').text(`${key}: ${value}`)
+        // });
+
+    });
+}
